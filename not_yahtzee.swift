80c8921948de13faec2f157e523749403686b433
archzaq <129307974+archzaq@users.noreply.github.com>
2024-05-05 13:59:35 -0500

    Update not_yahtzee.swift

--- not_yahtzee.swift
+++ not_yahtzee.swift
@@ -9,31 +9,35 @@ import Foundation
 
 let diceConstant: Int = 5
 
-func diceChoice(prompt: String) -> String? {
+// take user input as string
+func choice(prompt: String) -> String? {
     print(prompt, terminator: "")
-    if let choice = readLine() {
-        return choice
+    if let userChoice = readLine() {
+        return userChoice
     } else {
         return nil
     }
 }
 
+// takes a list, adds it to a dictonary with the value being the count
 func inspectArray(inputList: [Int]) -> [Int:Int] {
     var tracker: [Int:Int] = [:]
-    for char in inputList {
-        if let count = tracker[char] {
-            tracker[char] = count + 1
+    for number in inputList {
+        if let countValue = tracker[number] {
+            tracker[number] = countValue + 1
         } else {
-            tracker[char] = 1
+            tracker[number] = 1
         }
     }
     return tracker
 }
 
+// compares two dictonaries and returns true if the
+// value count in the first dictTracker is greater than or equal to the value count of the second
 func compareDict(rollTracker: [Int:Int], userTracker: [Int:Int]) -> Bool {
     for (numUser, countUser) in userTracker { // iterate through user input
         if let rollNum = rollTracker[numUser] {// if the rollTracker has the user input
-            if rollNum >= countUser {
+            if rollNum >= countUser { // if rollTracker value count is ge user input value count
                 continue
             } else {
                 return false
@@ -50,6 +54,7 @@ func compareDict(rollTracker: [Int:Int], userTracker: [Int:Int]) -> Bool {
 func diceRoll(dice: Int) -> [Int] {
     var output: [Int] = []
     if dice > 5 {
+        print("Too many dice")
         exit(1)
     }
     for _ in 0..<dice {
@@ -63,20 +68,19 @@ func diceRoll(dice: Int) -> [Int] {
 func firstTurn() -> [Int] {
     var diceKept: [Int] = []
     print()
-    print("\t- First Turn -")
+    print("\t- First Roll -")
     print()
     let firstTurnNumbers = diceRoll(dice: 5) // call diceRoll function
-    print("Roll: \t\(firstTurnNumbers.map { "\($0)" }.joined(separator: ", "))")
+    print("Roll: \t\(firstTurnNumbers.map { "\($0)" }.joined(separator: ", "))") // display roll
     print()
-    if let diceSelection = diceChoice(prompt: "Choose the dice you would like to keep, type done to keep all dice: ") {
+    if let diceSelection = choice(prompt: "Choose the dice you would like to keep, type done to keep all dice: ") { // get user input
         switch diceSelection {
-        case "":
+        case "": // if no user input, save no dice
             diceKept = []
-            break
-        case "done":
+        case "done": // if user input is done, save all dice
             print()
             diceKept = firstTurnNumbers
-        default:
+        default: // check user input for Int, check each Int for valid input against the current dice
             for numString in diceSelection {
                 if let num = Int(String(numString)) {
                     if firstTurnNumbers.contains(num) {
@@ -101,27 +105,26 @@ func firstTurn() -> [Int] {
 func secondTurn(diceKept: [Int]) -> [Int] {
     var diceKept2 = diceKept
     print()
-    print("\t- Second Turn -")
+    print("\t- Second Roll -")
     let newRoll = diceConstant - diceKept2.count
     let secondTurnNumbers = diceRoll(dice: newRoll).sorted() // call diceRoll function
     if !diceKept2.isEmpty {
         print()
-        print("Roll: \t\(diceKept2), \(secondTurnNumbers.map { "\($0)" }.joined(separator: ", "))")
+        print("Roll: \t\(diceKept2), \(secondTurnNumbers.map { "\($0)" }.joined(separator: ", "))") // if dice saved, show them with the roll
         print()
     } else {
         print()
-        print("Roll: \t\(secondTurnNumbers.map { "\($0)" }.joined(separator: ", "))")
+        print("Roll: \t\(secondTurnNumbers.map { "\($0)" }.joined(separator: ", "))") // if no dice saved, show roll
         print()
     }
-    if let diceSelection = diceChoice(prompt: "Choose the dice you would like to keep, type done to keep all dice: ") {
+    if let diceSelection = choice(prompt: "Choose the dice you would like to keep, type done to keep all dice: ") { // get user input
         switch diceSelection {
-        case "":
+        case "": // if no user input, save no dice
             diceKept2 = []
-            break
-        case "done":
+        case "done": // if user input is done, save all dice
             print()
             diceKept2 += secondTurnNumbers
-        default:
+        default: // check user input for Int, check each Int for valid input against the current dice
             var userKept2: [Int] = []
             for numString in diceSelection {
                 if let num = Int(String(numString)) {
@@ -130,8 +133,8 @@ func secondTurn(diceKept: [Int]) -> [Int] {
                         let rollDict2 = inspectArray(inputList: secondTurnNumbers)
                         let userDict2 = inspectArray(inputList: userKept2)
                         let prevDict = inspectArray(inputList: diceKept)
-                        let mergedDict = prevDict.merging(rollDict2) { (current, new) in
-                            current + new // Add the current value and the new value
+                        let mergedDict = prevDict.merging(rollDict2) { (current, new) in // barely understand the syntax of merging dictonaries
+                            current + new
                         }
                         if compareDict(rollTracker: mergedDict, userTracker: userDict2) {
                             diceKept2 = userKept2
@@ -150,7 +153,7 @@ func secondTurn(diceKept: [Int]) -> [Int] {
 func thirdTurn(diceKept2: [Int]) -> [Int] {
     var diceKept3 = diceKept2
     print()
-    print("\t- Third Turn -")
+    print("\t- Third Roll -")
     print()
     let newRoll2 = diceConstant - diceKept3.count
     let thirdTurnNumbers = diceRoll(dice: newRoll2) // call diceRoll function
@@ -160,26 +163,47 @@ func thirdTurn(diceKept2: [Int]) -> [Int] {
     return diceKept3.sorted()
 }
 
-func main() {
-    print("Welcome to not-Yahtzee!")
+func play(playerInt: Int) -> [Int] {
+    print()
+    print("------- Player \(playerInt) -------")
     let firstTurnResult = firstTurn().sorted()
     if firstTurnResult.count == 5 {
         print()
         print("Your turn is over. Final tally:", firstTurnResult.map { "\($0)" }.joined(separator: ", "))
         print()
-        exit(0)
+        return firstTurnResult
     }
     let secondTurnResult = secondTurn(diceKept: firstTurnResult).sorted()
     if secondTurnResult.count == 5 {
         print()
         print("Your turn is over. Final tally:", secondTurnResult.map { "\($0)" }.joined(separator: ", "))
         print()
-        exit(0)
+        return secondTurnResult
     }
     let thirdTurnResult = thirdTurn(diceKept2: secondTurnResult)
     print("Your turn is over. Final tally:", thirdTurnResult.map { "\($0)" }.joined(separator: ", "))
     print()
-    exit(0)
+    return thirdTurnResult
+}
+
+func main() {
+    print("Welcome to not-Yahtzee!")
+    print()
+    if let playersInput = choice(prompt: "How many players? ") {
+        switch playersInput {
+        case "":
+            exit(0)
+        default:
+            var playerInt: Int = 0
+            if playersInput.count == 1, let playerNum = Int(playersInput) {
+                for _ in 1...playerNum {
+                    playerInt += 1
+                    let _ = play(playerInt: playerInt)
+                    let _ = choice(prompt: "Press enter to continue")
+                }
+            }
+        }
+    }
 }
 
 main()
